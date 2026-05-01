797704a94f9beb613a2606e8d93971d12ed8be32
vondorn <vondoryn@gmail.com>
2026-05-02 02:50:45 +0700

    create microservice

--- consumer/src/main.ts
+++ consumer/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const appContext = await NestFactory.createApplicationContext(AppModule);
+
+  const configService = appContext.get(ConfigService);
+
+  const rmqUrl = configService.get<string>('RMQ_URL')!;
+  const rmqQueue = configService.get<string>('RMQ_QUEUE')!;
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: [rmqUrl],
+      queue: rmqQueue,
+      noAck: false,
+      queueOptions: {
+        durable: true,
+      },
+    },
+  });
+
+  await app.listen();
 }
 bootstrap();
